910a076af8217300d16ffb680403f329c0d843e6
Saachi Sen <saachisen@Saachis-MacBook-Pro.local>
2021-07-16 19:02:32 +0100

    fetching and using weather state

--- lib/details.dart
+++ lib/details.dart
@@ -1,29 +1,43 @@
 import 'package:flutter/material.dart';
 
-List get_stuff_list(String item) {
-  
+List get_stuff_list(String weatherState, String place) {
+// List get_stuff_list(String place) {
+
   List stuff_list = ['keys', 'wallet', 'phone', 'mask', 'hand sanitiser'];
 
-  if (item == 'Work') {
+  print (weatherState);
+
+  // expand list of stuff depending on today's weather 
+  if (weatherState == 'Heavy Rain') {
+    stuff_list.add('umbrella');
+  } else
+    if (weatherState == 'Clear') {
+    stuff_list.add('sunglasses');
+    stuff_list.add('sunscreen');
+  }
+
+
+  // expand list of stuff depending on where you're going today
+  if (place == 'Work') {
     stuff_list.add('work id card');
     stuff_list.add('latpop');
   } else
-  if (item == 'Party') {
+  if (place == 'Party') {
     stuff_list.add('lipstick');
     stuff_list.add('earrings');
     stuff_list.add('gift');
     stuff_list.add('wine');
   } else
-  if (item == 'Park') {
+  if (place == 'Park') {
     stuff_list.add('picnic blanket');
     stuff_list.add('insect repellent');
   } else
-  if (item == 'Gym') {
+  if (place == 'Gym') {
     stuff_list.add('water bottle');
     stuff_list.add('towel');
     stuff_list.add('change of clothes');
   } else
-  if (item == 'Day Out') {
+  if (place == 'Day Out') {
     stuff_list.add('cardigan');
     stuff_list.add('snack');
   } 
@@ -34,14 +48,20 @@ List get_stuff_list(String item) {
 
 class MyDetails extends StatelessWidget {
 
-  final item;
-  MyDetails(this.item);
+  final String itemState;
+  // final double itemTemp;
+  final String itemPlace;
+
+  // MyDetails(this.itemState, this.itemTemp, this.itemPlace);
+  MyDetails(this.itemState, this.itemPlace);
+  // MyDetails(this.itemPlace);
 
   @override
   Widget build(BuildContext context) {
-    final title = 'what to take to... $item';
+    final title = 'what to take to... $itemPlace';
 
-    List stuff_list = get_stuff_list(item);
+    // List stuff_list = get_stuff_list(itemPlace);
+    List stuff_list = get_stuff_list(itemState, itemPlace);
 
     return Scaffold(
         appBar: AppBar(
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,70 @@
+import 'package:my_app/model.dart';
 import 'package:my_app/details.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async' show Future;
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AllTheStuff',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        title: 'AllTheStuff',
+        key: null,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+Future<String> _loadRemoteData() async {
+  final response = await (http
+      .get(Uri.parse('https://www.metaweather.com/api/location/12586539/')));
+  if (response.statusCode == 200) {
+    print('response statusCode is 200');
+    return response.body;
+  } else {
+    print('Http Error: ${response.statusCode}!');
+    throw Exception('Invalid data source.');
+  }
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<DataSeries> fetchQuests() async {
+    String jsonString = await _loadRemoteData();
+    print('jsonString: ${jsonString}');
+
+    final jsonResponse = json.decode(jsonString);
+    print('jsonResponse: ${jsonResponse}');
+
+    DataSeries dataSeries = new DataSeries.fromJson(jsonResponse);
+
+    return dataSeries;
+  }
+
+  late Future<DataSeries> dataSeries;
+
   List<String> places = [
     'Work',
     'Party',
@@ -14,44 +73,44 @@ class MyApp extends StatelessWidget {
     'Day Out'
   ];
 
-  List<String> transports = [
-    'Walk',
-    'Tube',
-    'Drive',
-    'Cycle'
-  ];
-
-  List<String> durations = [
-    'Hour',
-    'Half Day',
-    'Day',
-  ];
+  @override
+  void initState() {
+    super.initState();
+    dataSeries = fetchQuests();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final title = 'AllTheStuff';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView.builder(
-          itemCount: places.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${places[index]}'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyDetails(places[index])));
-              },
-            );
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      body: FutureBuilder<DataSeries>(
+          future: dataSeries,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: places.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text('${places[index]}'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyDetails(
+                                  snapshot.data!.dataModel[0].state, // index 0 has today's weather
+                                  // snapshot.data!.dataModel[0].temp,
+                                  places[index])));
+                    },
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text("Error: ${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          }),
     );
   }
 }
